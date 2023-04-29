84bac9d993da5fffadd87d199c85a34ca3807c01
wabowen89 <wabowen89@gmail.com>
2023-04-28 17:16:09 -0700

    initial working of coding-concepts

--- coding-concepts.js
+++ coding-concepts.js
@@ -10,25 +10,30 @@
 const cohort = "Charlie 2023"
 // console.log(cohort.split(""))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: ['C', 'h', 'a', 'r', 'l', 'i', 'e', ' ', '2', '0', '2', '3']
+    // 'cohort' is a string datatype. '.split() is a mutator that can operate on either a string or an array that will create a new array. If you run '.split()' on a string, it will separate each character within that string in to its own element within a new array.
+
+// b) Verify and explain: ['C', 'h', 'a', 'r', 'l', 'i', 'e', ' ', '2', '0', '2', '3']
 
 // --------------------2) What will this log?
 
 const greeter = (name) => {
   `Hello, ${name}!`
 }
-// console.log(greeter("LEARN Student"))
+console.log(greeter("LEARN Student"))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: undefined
+    // 'greeter()' is a function that takes in a string as an argument ('name'). The function will then produce a string and, through the use of string interpolation, and the input string provided to its output. In order for a function to work, it must return something. This is accomplished with the code 'return'. As this function is missing the 'return' code, it will not return anything. As a result, it will log "undefined".
+
+// b) Verify and explain: undefined
 
 // --------------------3) What will this log?
 
 const multipliedByTwo = [4, 5, 6, 7, 8].map((number) => number * 2)
 // console.log(multipliedByTwo)
 
-// a) Your answer:
+// a) Your answer: [8, 10, 12, 14, 16]
+    // 
 // b) Verify and explain:
 
 // --------------------4) What will this log?
